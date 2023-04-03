9ae7946b82f20765d37e89321e61ec1dfa7d4176
Jo YangGi <jyg8033@gmail.com>
2023-04-04 00:02:43 +0900

    [Silver IV] Title: 스택, Time: 204 ms, Memory: 20648 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class MyStack {
+    ArrayList<Integer> MyStack = new ArrayList<>();
+
+    void push(int x) {
+        this.MyStack.add(x);
+    }
+
+
+
+    int pop() {
+        if (!this.MyStack.isEmpty()) {
+            int p = this.MyStack.get(this.MyStack.size() - 1);
+            this.MyStack.remove(this.MyStack.size() - 1);
+            return p;
+        } else return -1;
+    }
+    int size(){
+        return this.MyStack.size();
+    }
+    int empty(){
+        if(this.MyStack.size() == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    int top(){
+        if(this.MyStack.size() == 0){
+            return -1;
+        }else {
+            return this.MyStack.get(this.MyStack.size() - 1);
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        MyStack stack = new MyStack();
+        for (int i = 0; i < N; i++) {
+            String[] order = br.readLine().split(" ");
+            switch (order[0]){
+                case "push":
+                    stack.push(Integer.parseInt(order[1]));
+                    break;
+                case "pop":
+                    sb.append(stack.pop()).append('\n');
+                    break;
+                case "size":
+                    sb.append(stack.size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(stack.empty()).append('\n');
+                    break;
+                case "top":
+                    sb.append(stack.top()).append('\n');
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
