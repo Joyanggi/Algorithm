74f542fe44eb81acd7cb2b809d38fb20ecd284b4
Jo YangGi <jyg8033@gmail.com>
2023-09-06 13:59:11 +0900

    [Bronze V] Title: 구구단, Time: 164 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = scanner.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= 9; i++){
-            System.out.println( N + " * " + i + " = " + (N*i));
+        for(int i = 1; i < 10; i++){
+            System.out.println(N + " * " + i + " = " + (N*i));
         }
     }
 }
