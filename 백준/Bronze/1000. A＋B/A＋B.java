f63bdc63b851a4f0025b37db913d81167bfd4b2b
양기 <jyg8033@gmail.com>
2024-10-13 16:08:29 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,16 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        
+
+        int A;
+        int B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
         System.out.println(A + B);
+
+
     }
 }
