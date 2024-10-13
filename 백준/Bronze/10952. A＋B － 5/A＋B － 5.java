a66debafed27eea9bd3a309bf0066ba04f27b429
양기 <jyg8033@gmail.com>
2024-10-13 21:18:40 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -7,19 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        String str;
         StringTokenizer st;
-        
-        while(true){
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            if(a == 0 && b == 0){
-                break;
-            }
-            sb.append((a + b)).append('\n');
-            
+
+        while(!(str = br.readLine()).equals("0 0")) {
+            st = new StringTokenizer(str, " ");
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(A + B);
+
         }
-        System.out.println(sb);
+
     }
 }
