b762eabc406dc415213eef91fec4f8624b1b8c40
양기 <jyg8033@gmail.com>
2024-10-14 20:33:46 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -4,10 +4,12 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
         String str;
-        while((str = br.readLine()) != null){
+        
+        while((str = br.readLine()) != null) {
             System.out.println(str);
         }
+        
     }
 }
