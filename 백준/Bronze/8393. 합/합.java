d41ad9a721a30f826ee33245875972f9cc5b777f
양기 <jyg8033@gmail.com>
2024-10-15 22:48:51 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,13 +5,15 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
         int sum = 0;
-
-        for(int i = 1; i <= n; i++){
+        
+        for(int i = 1; i <= n; i++) {
             sum += i;
         }
+
         System.out.println(sum);
     }
 }
