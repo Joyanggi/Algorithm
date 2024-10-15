2cf33695ddfc470c03800d25fbf1fdb7c0189985
양기 <jyg8033@gmail.com>
2024-10-15 22:28:02 +0900

    [Bronze V] Title: N 찍기, Time: 688 ms, Memory: 30176 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -5,10 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         
-        for(int i = 1; i <= N; i++){
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= T; i++) {
             System.out.println(i);
         }
+        
     }
 }
