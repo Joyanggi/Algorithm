d514dfce499fe19fd3f1e5a762c0e2b9a0940939
양기 <jyg8033@gmail.com>
2024-10-15 22:34:28 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -5,11 +5,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i < 10; i++){
-            System.out.println(N + " * " + i + " = " + (N*i));
+        for(int i = 1; i <= 9; i++) {
+            sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append('\n');
         }
+        System.out.println(sb);
     }
 }
