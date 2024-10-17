a2f3b5c985333d6f25f0195b036691c2dc978ec9
양기 <jyg8033@gmail.com>
2024-10-17 20:42:20 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 172 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int a = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= a; i++) {
-            for(int j = i; j <= a; j++) {
+        for(int i = N; i > 0; i--) {
+            System.out.print("*");
+            for(int j = 0; j < i-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
     }
 }
